08ea39db2c24dc3a0646388fba6cfa4239fcfd16
dylan <dylanglandry@gmail.com>
2023-06-04 14:40:42 -0400

    add pawns and advancing them to tiles, and using connections

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,39 @@
+pub struct Pawn<'a> {
+    pub tile: &'a Tile,
+}
+
+impl<'a> Pawn<'a> {
+    pub fn new<'b: 'a> (tile: &'a Tile) -> Self {
+        Self {
+            tile
+        }
+    }
+
+    pub fn advance<'b: 'a>(&mut self, end_tile: &'b Tile, connections: &'b [Connection]) {
+        let mut target_end_tile = end_tile;
+        while let Some(connection_to_travel) = connections.iter().find(
+            |conn| std::ptr::eq(conn.start, target_end_tile)
+        ) {
+            target_end_tile = connection_to_travel.end;
+        }
+        self.tile = target_end_tile;
+    }
+}
+
+impl<'a> std::fmt::Display for Pawn<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write![f, "Pawn at tile {}.", self.tile.position]
+    }
+}
+
 pub struct Tile {
     pub position: i32,
+}
+
+impl std::fmt::Display for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write![f, "Tile with position {}", self.position]
+    }
 }
 
 impl Tile {
@@ -14,14 +48,25 @@ impl Tile {
 }
 
 pub struct Connection<'a> {
-    start: &'a Tile,
-    end: &'a Tile,
+    pub start: &'a Tile,
+    pub end: &'a Tile,
+    pub kind: ConnectionKind
 }
 
 #[derive(PartialEq, Eq)]
 pub enum ConnectionKind {
     Slide,
     Ladder
+}
+
+impl std::fmt::Display for ConnectionKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let label = match self {
+            ConnectionKind::Slide => "slide",
+            ConnectionKind::Ladder => "ladder",
+        };
+        write!(f, "{}", label)
+    }
 }
 
 impl<'a> Connection<'a> {
@@ -39,6 +84,7 @@ impl<'a> Connection<'a> {
         }
 
         Self {
+            kind,
             start,
             end
         }
@@ -47,7 +93,7 @@ impl<'a> Connection<'a> {
 
 impl<'a> std::fmt::Display for Connection<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Connection from tile {} to tile {}.", self.start.position, self.end.position)
+        write!(f, "{} from tile {} to tile {}.", self.kind, self.start.position, self.end.position)
     }
 }
 
@@ -90,6 +136,52 @@ mod test {
                 let lower_tile = Tile { position: 0 };
                 let higher_tile = Tile { position: 1 };
                 Connection::new(ConnectionKind::Ladder, &higher_tile, &lower_tile);
+            }
+        }
+    }
+
+    mod pawn {
+        mod advance {
+            use super::super::super::*;
+
+            #[test]
+            fn moves_pawn_to_tile() {
+                let start_tile = Tile::new(0);
+                let end_tile = Tile::new(1);
+                let mut pawn = Pawn::new(&start_tile);
+                let connections: Vec<Connection> = Vec::new();
+                pawn.advance(&end_tile, &connections);
+                assert!(std::ptr::eq(pawn.tile, &end_tile));
+            }
+
+            #[test]
+            fn follows_connections_until_they_stop() {
+                let tile_1 = Tile::new(0);
+                let tile_2 = Tile::new(1);
+                let tile_3 = Tile::new(2);
+                let tile_4 = Tile::new(3);
+                let mut pawn = Pawn::new(&tile_1);
+                let connections: Vec<Connection> = vec![
+                    Connection::new(ConnectionKind::Ladder, &tile_2, &tile_3),
+                    Connection::new(ConnectionKind::Ladder, &tile_3, &tile_4),
+                ];
+                pawn.advance(&tile_2, &connections);
+                assert!(std::ptr::eq(pawn.tile, &tile_4));
+            }
+
+            #[test]
+            fn follows_ladder_and_slide_connections_until_they_stop() {
+                let tile_1 = Tile::new(0);
+                let tile_2 = Tile::new(1);
+                let tile_3 = Tile::new(2);
+                let tile_4 = Tile::new(3);
+                let mut pawn = Pawn::new(&tile_1);
+                let connections: Vec<Connection> = vec![
+                    Connection::new(ConnectionKind::Ladder, &tile_2, &tile_4),
+                    Connection::new(ConnectionKind::Slide, &tile_4, &tile_3),
+                ];
+                pawn.advance(&tile_2, &connections);
+                assert!(std::ptr::eq(pawn.tile, &tile_3));
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,34 @@ fn main() {
         println!("roll {}: {}", i+1, result);
     }
 
-    let tile_1 = board::Tile::new(0);
-    let _tile_2 = board::Tile::new(1);
-    let tile_3 = board::Tile::new(2);
-    let connection = board::Connection::new(board::ConnectionKind::Slide, &tile_3, &tile_1);
-    println!("{}", connection);
+    let tiles = vec![
+        board::Tile::new(0),
+        board::Tile::new(1),
+        board::Tile::new(2),
+        board::Tile::new(3),
+    ];
+
+    let connections = vec![
+        board::Connection::new(
+            board::ConnectionKind::Ladder,
+            tiles.get(1).unwrap(),
+            tiles.get(3).unwrap(),
+        )
+    ];
+
+    let mut pawn = board::Pawn::new(tiles.get(0).unwrap());
+
+    for tile in &tiles {
+        println!("{}", tile);
+    }
+
+    for conn in &connections {
+        println!("{}", conn);
+    }
+
+    println!("{}", pawn);
+
+    pawn.advance(tiles.get(1).unwrap(), &connections);
+
+    println!("{}", pawn);
 }
