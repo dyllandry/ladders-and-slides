26fe046896ee5e62bed3df73eff12c7b0b446921
dylan <dylanglandry@gmail.com>
2023-06-16 21:45:50 -0400

    simplify design; remove Rc; use more i32; move board into ladders_and_slides module

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
+use ladders_and_slides::LaddersAndSlides;
+
 mod dice;
-mod board;
+mod ladders_and_slides;
 
 fn main() {
-    let board = board::Board::new(20);
+    let num_tiles = 40;
+    let num_players = 2;
+    let mut game = LaddersAndSlides::new(num_tiles, num_players);
 
-    let mut pawn = board::Pawn::new(
-        board.tiles.get(0).unwrap()
-    );
-
-    let num_ladders = board.connections.iter().filter(|conn| conn.kind == board::ConnectionKind::Ladder).count() as i32;
-    let num_slides = board.connections.iter().filter(|conn| conn.kind == board::ConnectionKind::Slide).count() as i32;
-    println!(
-        "Made a board with {} tiles, {} ladders, and {} slides.",
-        board.tiles.len(),
-        num_ladders,
-        num_slides
-    );
-
-    let first_ladder = board.connections.iter().find(|conn| conn.kind == board::ConnectionKind::Ladder).unwrap();
-
-    println!(
-        "First ladder moves from tile {} to tile {}.",
-         first_ladder.start.position,
-         first_ladder.end.position
-     );
-
-    println!("Moving pawn from tile {} to tile {}.", pawn.tile.position, first_ladder.start.position);
-
-    pawn.advance(&first_ladder.start, &board.connections);
-
-    println!("Pawn ended up at tile {}!", pawn.tile.position);
+    loop {
+        game.take_turn();
+    }
 }
